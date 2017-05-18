00fc6470d5e412008ddacc4edc934c03b59ad5c6
Wilson Arce <wdarcedev@gmail.com>
2017-05-18 07:56:36 -0500

    Redefiniendo idioma ingles para devise

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Colprov
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    config.i18n.default_locale = :es
+    #config.i18n.default_locale = :es
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
